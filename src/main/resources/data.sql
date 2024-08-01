2e8c1fcc8322a7ccbbd103bb221da96000982254
Gabriela <gabriellagb17@gmail.com>
2024-08-01 10:52:19 +0300

    Docker support

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, email) VALUES
+('John', 'Doe', 'john.doe@example.com'),
+('Jane', 'Smith', 'jane.smith@example.com'),
+('Michael', 'Brown', 'michael.brown@example.com'),
+('Emily', 'Davis', 'emily.davis@example.com'),
+('William', 'Johnson', 'william.johnson@example.com');
